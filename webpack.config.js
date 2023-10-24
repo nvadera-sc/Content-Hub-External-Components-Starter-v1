4d42d4a8270ca5b7fa061f51b968d4985cf67cdf
Nicky Vadera <83960839+nvadera-sc@users.noreply.github.com>
2023-10-24 11:20:43 +0100

    Removed express in favour of webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,69 @@
+const path = require("path");
+const glob = require("glob");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-    entry: {
-        Simple: './src/Simple/Index.ts',
-        SimpleReact: './src/SimpleReact/Index.jsx',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: ['babel-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js']
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/public',
-        library: {
-            type: "module"
-        }
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/static', to: 'static' }
+function getConfig() {
+    const entries = glob.globSync("./src/!(_*)/Index.{js,ts,jsx,tsx}").map(entryPath => [path.basename(path.dirname(entryPath)), `./${entryPath}`])
+    return {
+        entry: Object.fromEntries(entries),
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    use: ['babel-loader'],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                    exclude: /node_modules/,
+                },
             ]
-        })
-    ],
-    optimization: {
-        minimize: false,
-        minimizer: [
-          new TerserPlugin({
-            exclude: /static/,
-          }),
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.jsx', '.js'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public'),
+            library: {
+                type: "module"
+            }
+        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'src/static', to: 'static' }
+                ]
+            })
         ],
-    },
-    experiments: {
-        outputModule: true
-    }
-};
+        devServer: {
+            compress: true,
+            hot: false,
+            port: 3000,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            watchFiles: ['./src/**/*'],
+        },
+        optimization: {
+            minimize: false,
+            minimizer: [
+            new TerserPlugin({
+                exclude: /static/,
+            }),
+            ],
+        },
+        experiments: {
+            outputModule: true
+        }
+    };
+}
+
+module.exports = getConfig();
